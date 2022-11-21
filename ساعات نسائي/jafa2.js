fc232781d352a697c38f0391ed535b0f65c68ca6
asmaaabdahrhman <118649886+asmaaabdahrhman@users.noreply.github.com>
2022-11-21 19:37:43 +0300

    Add files via upload

---
+++ ساعات نسائي/jafa2.js
@@ -0,0 +1,7 @@
+function fun1(img){
+    img.style.width="100px";
+}
+function fun2(img){
+    img.style.width="200px"; 
+}
+setTimeout(function(){document.getElementById('p1').innerHTML="king Watches";},1000)
